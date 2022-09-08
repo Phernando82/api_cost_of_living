71bef2d539cc1399634311c794308dab9b3bb342
Fernando Valverde <nandovalverde@gmail.com>
2022-09-08 18:25:21 +0100

    'Procfile'

--- numbeo_spider/numbeo_spider/spiders/numbeo.py
+++ numbeo_spider/numbeo_spider/spiders/numbeo.py
@@ -6,7 +6,7 @@ class NumbeoSpider(scrapy.Spider):
     # request
     def start_requests(self):
         # Definir as urls para varrer
-        city = 'Sao-Paulo'
+        city = 'Lisbon'
         urls = [f'https://www.numbeo.com/cost-of-living/in/{city}']
 
         for url in urls:
